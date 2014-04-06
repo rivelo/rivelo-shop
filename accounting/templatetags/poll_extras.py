e2d30c7d8927f91dcb5eb86a0337002b98211b29
igor.panchuk <igor.panchuk@226ac589-d54f-05a2-4dea-f8b8e0e92cb6>
2014-04-06 06:39:46 +0000

    fix pricelist bug

--- accounting/templatetags/poll_extras.py
+++ accounting/templatetags/poll_extras.py
@@ -94,3 +94,12 @@ def lenght(value):
     """
     return len(value)
 
+
+@register.filter("truncate_chars")
+def truncate_chars(value, max_length):
+    if len(value) > max_length:
+        truncd_val = value[:max_length]
+        if not len(value) == max_length+1 and value[max_length+1] != " ":
+            truncd_val = truncd_val[:truncd_val.rfind(" ")]
+        return  truncd_val + "..."
+    return value
